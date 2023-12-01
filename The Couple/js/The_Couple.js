5633b307dc7f35d9a5150892dbde8847549b6182
diaa qassem <107882522+diaaqassem@users.noreply.github.com>
2023-12-01 02:00:59 +0200

    Add files via upload

---
+++ The Couple/js/The_Couple.js
@@ -0,0 +1,9 @@
+// var div = document.getElementById("mouse1");
+
+// document.addEventListener("mousemove",function(event){
+//    div.style.position="absolute";
+//    div.style.top=event.clientY;
+//    div.style.left=event.clientX;
+
+
+// })
